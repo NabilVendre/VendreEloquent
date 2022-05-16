cd3a04f62e4ab08eab0a44291b239e85250b63e3
Daniel Lögren <daniel@vendre.se>
2022-05-16 16:36:06 +0200

    Patch: Remove unused code

--- src/Connections/MySQLiConnection.php
+++ src/Connections/MySQLiConnection.php
@@ -610,19 +610,6 @@ class MySQLiConnection extends Connection implements ConnectionInterface
         return strtr($str, $this->mysqlEscapeChars);
     }
 
-    protected function escapeImprovded($inp)
-    {
-        if (is_array($inp)) {
-            return array_map(__METHOD__, $inp);
-        }
-
-        if (!empty($inp) && is_string($inp)) {
-            return str_replace(['\\', "\0", "\n", "\r", "'", '"', "\x1a"], ['\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'], $inp);
-        }
-
-        return $inp;
-    }
-
     /**
      * @param  mixed $value
      * @param string $type
